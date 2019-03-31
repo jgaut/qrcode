8cc6ed73537ed23028ec648e6ab4a3741b909784
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-03-31 20:55:34 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Auth from '@aws-amplify/auth';
 import awsconfig from './aws-exports';
 import SignInUp from './signinup';
 import SignInConfirm from './signinconfirm';
-import { NavLink,Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom'
+import { NavLink,Route, Link, BrowserRouter as Router, Switch, Redirect } from 'react-router-dom'
 
 // retrieve temporary AWS credentials and sign requests
 Auth.configure(awsconfig);
