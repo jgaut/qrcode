438a0b29c4a6e9949ea491580d45a9ecdbba864a
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-03-31 21:38:24 +0200

    Update noauth.js

--- src/noauth.js
+++ src/noauth.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+import Auth from '@aws-amplify/auth';
+import { NavLink,Route, Link, BrowserRouter as Router, Switch, Redirect } from 'react-router-dom'
 
 class NoAuth extends Component {
 
   constructor(props) {
     super(props);
+  }
+
+  componentWillMount(){
+    Auth.currentAuthenticatedUser({bypassCache: false})
+    .then((user) => {
+      console.log(user);
+      this.props.history.push('/signinup');
+      }
+    ).catch((err) => {
+      console.log(err);
+      this.props.history.push('/signinup');
+      }
+    )  
   }
 
   render() {
