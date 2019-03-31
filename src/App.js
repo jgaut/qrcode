79705db63a59226c375175cd3db967a367f160d6
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-03-31 21:40:15 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,16 +5,12 @@ import awsconfig from './aws-exports';
 import SignInUp from './signinup';
 import SignInConfirm from './signinconfirm';
 import NoAuth from './noauth';
-import { NavLink,Route, Link, BrowserRouter as Router, Switch, Redirect } from 'react-router-dom'
+import { Route, Link, BrowserRouter as Router, Switch, Redirect } from 'react-router-dom'
 
 // retrieve temporary AWS credentials and sign requests
 Auth.configure(awsconfig);
 
 class App extends Component {
-
-  constructor(props) {
-    super(props);
-  }
 
   render() {
 
