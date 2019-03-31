d03177d6b3ab7c5a2253f2cfaa75e972da9fb446
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-03-31 21:37:01 +0200

    Update noauth.js

--- src/noauth.js
+++ src/noauth.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import Auth from '@aws-amplify/auth';
-import { NavLink,Route, Link, BrowserRouter as Router, Switch, Redirect } from 'react-router-dom'
 
 class NoAuth extends Component {
 
