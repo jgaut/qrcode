89bfb6f391e500b8fa716f9d7643b235e0ec0755
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-03-31 21:34:01 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,9 +25,7 @@ class App extends Component {
           <Switch>
             <Route exact path="/signinup" component={SignInUp} />
             <Route exact path="/signinconfirm" component={SignInConfirm} />
-            <Route path='/' render={() => {
-              return(<Redirect to="/noauth" />);  
-            }}/> 
+            <Route path='/' render={() => (<Redirect to="/noauth" />)}/> 
           </Switch>
         </header>
       </div>
