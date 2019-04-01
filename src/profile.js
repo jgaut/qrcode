3e6beb245cabfaf819aaea326768e50b532517da
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-04-01 10:14:26 +0200

    Create profile.js

---
+++ src/profile.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import Auth from '@aws-amplify/auth';
+
+class Profile extends Component {
+
+  componentWillMount(){  
+  }
+
+  render() {
+
+    return (
+      <div>My profile</div>
+    );
+      
+  }
+}
+
+export default Profile;
