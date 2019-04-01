7e91e2ea08b7301c48c0dd763f7a2552a960aff6
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-04-01 10:14:23 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import awsconfig from './aws-exports';
 import SignInUp from './signinup';
 import SignInConfirm from './signinconfirm';
 import NoAuth from './noauth';
+import Profile from './profile';
 import { Route, BrowserRouter as Router, Switch, Redirect } from 'react-router-dom'
 
 // retrieve temporary AWS credentials and sign requests
@@ -22,6 +23,7 @@ class App extends Component {
             <Route exact path="/signinup" component={SignInUp} />
             <Route exact path="/signinconfirm" component={SignInConfirm} />
             <Route exact path="/noauth" component={NoAuth} />
+            <Route exact path="/profile" component={Profile} />
             <Route path='/' render={() => (<Redirect to="/noauth" />)}/> 
           </Switch>
         </header>
