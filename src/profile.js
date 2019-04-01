4bf55526e22268db5fc3cad66802cb2adf613282
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-04-01 14:53:11 +0200

    Update profile.js

--- src/profile.js
+++ src/profile.js
@@ -18,7 +18,10 @@ class Profile extends Component {
 
       Auth.currentAuthenticatedUser({
     bypassCache: false  // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
-}).then(user => console.log(JSON.stringify(user['clientId'])))
+}).then(user => {
+  user2=JSON.stringify(user);
+  console.log(user2.clientId);:
+})
 .catch(err => console.log(err));
 
  	}
