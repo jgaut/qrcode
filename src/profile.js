d764c4c7e4df21178e560941df80ede4a59322e0
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-04-01 15:25:46 +0200

    Update profile.js

--- src/profile.js
+++ src/profile.js
@@ -25,6 +25,7 @@ class Profile extends Component {
     bypassCache: false  // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
 }).then(user => {
   this.state.sub = user.attributes.sub;
+  this.Load();
   //console.log(this.state.sub);
  
 })
