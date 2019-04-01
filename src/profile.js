dbcfec8d07806a9372e7713d8bcc6850fdb330ec
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-04-01 14:55:01 +0200

    Update profile.js

--- src/profile.js
+++ src/profile.js
@@ -10,7 +10,8 @@ class Profile extends Component {
 	    this.state = {
 	      nom: '',
 	      prenom: '',
-	      age: ''
+	      age: '',
+        user:{},
 	    };
 	    
 	    this.handleChange = this.handleChange.bind(this);
@@ -19,8 +20,8 @@ class Profile extends Component {
       Auth.currentAuthenticatedUser({
     bypassCache: false  // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
 }).then(user => {
-  var user2=JSON.stringify(user);
-  console.log(user2.clientId);
+  this.state.user = user ;
+  console.log(this.state.user);
 })
 .catch(err => console.log(err));
 
