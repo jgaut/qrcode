ef0a09066bf3043b35b79d43121afdc9eda501f8
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-04-01 11:14:58 +0200

    Update profile.js

--- src/profile.js
+++ src/profile.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
-import Auth from '@aws-amplify/auth';
 
 class Profile extends Component {
 
   componentWillMount(){  
   }
 
+  handleChange(event) {
+    //console.log(event.target);
+
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
   render() {
 
     return (
-      <div>My profile</div>
+    	<div>My profile
+      		<label>Nom</label> <input type="text" name="nom" value={this.state.nom} onChange={this.handleChange}/>
+      		<label>Prénom</label> <input type="text" name="prenom" value={this.state.prenom} onChange={this.handleChange}/>
+      		<label>Age</label> <input type="text" name="age" value={this.state.age} onChange={this.handleChange}/>
+		</div>
     );
       
   }
