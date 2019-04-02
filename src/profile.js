1ade436c125b512704f9e23f692c95e24a4af8f6
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-04-02 09:55:15 +0200

    Update profile.js

--- src/profile.js
+++ src/profile.js
@@ -5,7 +5,7 @@ import awsmobile from './aws-exports';
 import QRCode from 'qrcode.react';
 import ls from 'local-storage';
 import Mnemonic from 'bitcore-mnemonic';
-const openpgp = require('openpgp');
+import openpgp from 'openpgp';
 
 Amplify.configure(awsmobile);
 
