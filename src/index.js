b245c7b9c191a3c360afaccb3294e950440c60a6
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-04-02 09:49:50 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import * as openPgp from './openpgp.worker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
@@ -11,4 +10,3 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-openPgp.unregister();
