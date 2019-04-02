daaa5debbfb515c588ac16b5637c4ad29aa9ffbe
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-04-02 15:30:58 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import * as openpgp from 'openpgp/dist/openpgp.worker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
