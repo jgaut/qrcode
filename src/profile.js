10400c8871b9674720aa51c0fb6b85a9dbbb7186
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-04-03 15:40:40 +0200

    Update profile.js

--- src/profile.js
+++ src/profile.js
@@ -148,7 +148,7 @@ class Profile extends Component {
 
       console.log("this.copyState :" + JSON.stringify(this.copyState));
       while(this.cpt>0){
-        setTimeout(()=>{}), 1000);
+        setTimeout(()=>{}, 1000);
       }
       Storage.put(this.sub+".json", JSON.stringify(this.copyState), {
         level: 'public',
