d013653479eeef056330c8a2c0e6f88225b32392
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-04-03 17:32:34 +0200

    Update profile.js

--- src/profile.js
+++ src/profile.js
@@ -172,8 +172,8 @@ class Profile extends Component {
       console.log("wait for cpt : " + this.cpt);
       setTimeout(this.waitForSave, 1000);
     } else {
-      console.log("Save this.copyState :" + this.copyState.toString());
-            Storage.put(this.sub+".json", this.copyState.toString(), {
+      console.log("Save this.copyState :" + JSON.parse(this.copyState));
+            Storage.put(this.sub+".json", JSON.parse(this.copyState), {
           level: 'public',
           contentType: 'text/plain'
         })
