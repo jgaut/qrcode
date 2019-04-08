87ab5340986eaf59bcb8dd150a8629bedbbbb474
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-04-08 20:51:10 +0200

    Update profile.js

--- src/profile.js
+++ src/profile.js
@@ -205,7 +205,7 @@ class Profile extends Component {
     var tmp='';
     for (var key in tab) {
       console.log(key, tab[key]);
-      var tmp2 = ReactDOM.render(<label>{key} :: {tab[key]}</label>);
+      var tmp2 = render(<label>{key} :: {tab[key]}</label>);
       tmp = tmp + "<br></br> " +tmp2;
       }
 
