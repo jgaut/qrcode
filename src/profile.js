6453b6a11f0867bc456b5d27b1fbb14cdfceb5e5
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-04-09 17:48:22 +0200

    Update profile.js

--- src/profile.js
+++ src/profile.js
@@ -219,13 +219,17 @@ class Profile extends Component {
     return (
     	<div>
     	<h1>My profile</h1><br></br>
+      <tr>
+        <td>
     		<label>Nom</label> <input type="text" name="nom" value={this.state.nom} onChange={this.handleChange} onBlur={this.Save}/><br></br>
     		<label>Prénom</label> <input type="text" name="prenom" value={this.state.prenom} onChange={this.handleChange} onBlur={this.Save}/><br></br>
     		<label>Age</label> <input type="text" name="age" value={this.state.age} onChange={this.handleChange} onBlur={this.Save}/><br></br>
         <label>Groupe sangin</label> <input type="text" name="gs" value={this.state.gs} onChange={this.handleChange} onBlur={this.Save}/><br></br>
         <label>Notes</label> <textarea name="notes" rows="5" value={this.state.notes} onChange={this.handleChange} onBlur={this.Save}/><br></br>
         <button onClick={this.LogOut}>Logout</button><br></br>
-         <button onClick={this.ChangeMasterKey}>ChangeMasterKey</button><br></br>
+        <button onClick={this.ChangeMasterKey}>ChangeMasterKey</button><br></br>
+        </td>
+        <td>
         <QRCode value={qrcodeValue} size={size} includeMargin={true}/><br></br>
         <label>{this.code}</label><br></br>
         <a target="_BLANK" title="QRCode link" href={qrcodeValue}>
@@ -234,6 +238,8 @@ class Profile extends Component {
         <a target="_BLANK" title="Data link" href={dataLink}>
           Lien vers le fichier de données
         </a><br></br>
+        </td>
+      </tr>
 		</div>
     );
       
