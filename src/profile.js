38082895f502b261923eba2712ea661cc7f87145
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-04-09 17:54:11 +0200

    Update profile.js

--- src/profile.js
+++ src/profile.js
@@ -221,13 +221,24 @@ class Profile extends Component {
     	<h1>My profile</h1><br></br>
       <tr>
         <td valign="top">
-    		<label>Nom</label> <input type="text" name="nom" value={this.state.nom} onChange={this.handleChange} onBlur={this.Save}/><br></br>
-    		<label>Prénom</label> <input type="text" name="prenom" value={this.state.prenom} onChange={this.handleChange} onBlur={this.Save}/><br></br>
+    		<tr>
+        <td>
+        <label>Nom</label> 
+        </td>
+        <td>
+        <input type="text" name="nom" value={this.state.nom} onChange={this.handleChange} onBlur={this.Save}/><br></br>
+    		</td>
+        </tr>
+        <tr>
+        <td>
+        <label>Prénom</label> <input type="text" name="prenom" value={this.state.prenom} onChange={this.handleChange} onBlur={this.Save}/><br></br>
     		<label>Age</label> <input type="text" name="age" value={this.state.age} onChange={this.handleChange} onBlur={this.Save}/><br></br>
         <label>Groupe sangin</label> <input type="text" name="gs" value={this.state.gs} onChange={this.handleChange} onBlur={this.Save}/><br></br>
         <label>Notes</label> <textarea name="notes" rows="5" value={this.state.notes} onChange={this.handleChange} onBlur={this.Save}/><br></br>
         <button onClick={this.LogOut}>Logout</button><br></br>
         <button onClick={this.ChangeMasterKey}>ChangeMasterKey</button><br></br>
+        </td>
+        </tr>
         </td>
         <td valign="top">
         <QRCode value={qrcodeValue} size={size} includeMargin={true}/><br></br>
