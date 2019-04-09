d6e449f4ea521a2b609ea6c40973e9e9bf54f331
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-04-09 17:58:39 +0200

    Update profile.js

--- src/profile.js
+++ src/profile.js
@@ -221,26 +221,64 @@ class Profile extends Component {
     	<h1>My profile</h1><br></br>
       <tr>
         <td valign="top">
+
     		<tr>
         <td>
         <label>Nom</label> 
         </td>
         <td>
-        <input type="text" name="nom" value={this.state.nom} onChange={this.handleChange} onBlur={this.Save}/><br></br>
+        <input type="text" name="nom" value={this.state.nom} onChange={this.handleChange} onBlur={this.Save}/>
     		</td>
         </tr>
+
         <tr>
         <td>
-        <label>Prénom</label> <input type="text" name="prenom" value={this.state.prenom} onChange={this.handleChange} onBlur={this.Save}/><br></br>
-    		<label>Age</label> <input type="text" name="age" value={this.state.age} onChange={this.handleChange} onBlur={this.Save}/><br></br>
-        <label>Groupe sangin</label> <input type="text" name="gs" value={this.state.gs} onChange={this.handleChange} onBlur={this.Save}/><br></br>
-        <label>Notes</label> <textarea name="notes" rows="5" value={this.state.notes} onChange={this.handleChange} onBlur={this.Save}/><br></br>
-        <button onClick={this.LogOut}>Logout</button><br></br>
-        <button onClick={this.ChangeMasterKey}>ChangeMasterKey</button><br></br>
+        <label>Prénom</label> 
+        </td>
+        <td>
+        <input type="text" name="prenom" value={this.state.prenom} onChange={this.handleChange} onBlur={this.Save}/>
+    		</td>
+        </tr>
+
+        <tr>
+        <td>
+        <label>Age</label> 
+        </td>
+        <td>
+        <input type="text" name="age" value={this.state.age} onChange={this.handleChange} onBlur={this.Save}/>
         </td>
         </tr>
+
+        <tr>
+        <td>
+        <label>Groupe sangin</label> 
+        </td>
+        <td>
+        <input type="text" name="gs" value={this.state.gs} onChange={this.handleChange} onBlur={this.Save}/>
+        </td>
+        </tr>
+
+        <tr>
+        <td>
+        <label>Notes</label>
+        </td>
+        <td>
+        <textarea name="notes" rows="5" value={this.state.notes} onChange={this.handleChange} onBlur={this.Save}/>
+        </td>
+        </tr>
+
+        <tr>
+        <td>
+        <button onClick={this.LogOut}>Logout</button>
+        </td>
+        <td>
+        <button onClick={this.ChangeMasterKey}>ChangeMasterKey</button>
+        </td>
+        </tr>
+
         </td>
         <td valign="top">
+
         <QRCode value={qrcodeValue} size={size} includeMargin={true}/><br></br>
         <label>{this.code}</label><br></br>
         <a target="_BLANK" title="QRCode link" href={qrcodeValue}>
@@ -249,6 +287,7 @@ class Profile extends Component {
         <a target="_BLANK" title="Data link" href={dataLink}>
           Lien vers le fichier de données
         </a><br></br>
+        
         </td>
       </tr>
 		</div>
