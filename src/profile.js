54a50d80d078bd87c779cb4718fee74e07a3b833
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-04-14 18:46:01 +0200

    Update profile.js

--- src/profile.js
+++ src/profile.js
@@ -410,6 +410,8 @@ const App = () => (
   </Styles>
 )
 
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
 const onSubmit = async values => {
   await sleep(300)
   window.alert(JSON.stringify(values, 0, 2))
