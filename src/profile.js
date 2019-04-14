16ae686a483f80f49358d1f56404064fa59705f5
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-04-14 20:49:17 +0200

    Update profile.js

--- src/profile.js
+++ src/profile.js
@@ -231,7 +231,7 @@ class Profile extends Component {
               <div>
                 <label>{myKey}</label>
                 <input {...input} key={Math.random()} name={myKey} type="text" placeholder="Username" value={this.state[myKey]} 
-              onChange={this.handleChange} 
+               
               onBlur={this.Save}
               />
                 {meta.error && meta.touched && <span>{meta.error}</span>}
