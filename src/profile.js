0e665dd2ac2f0934b08bb2f2b01087561479f848
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-04-15 17:06:33 +0200

    Update profile.js

--- src/profile.js
+++ src/profile.js
@@ -275,10 +275,16 @@ class Profile extends Component {
     this.dataLink = "https://s3-eu-west-1.amazonaws.com/qrcodebbae64624e2c4eaa95c85650b48ffb6c/public/"+this.sub+".json";
 
     return (
-    <div style={{marginLeft: "auto",
+    <div
+          style={{
+            fontFamily:
+              "-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif",
+            marginLeft: "auto",
             marginRight: "auto",
             maxWidth: "900px",
-            padding: "1em"}}>
+            padding: "1em"
+          }}
+        >
     <h1>Profile</h1>
     <Table>  	 
     {this.processItems(this.state)}
