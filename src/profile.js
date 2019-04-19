e49cd2c30a10c8533eacd08e53e8eb0c0f26cf54
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-04-19 14:34:20 +0200

    Update profile.js

--- src/profile.js
+++ src/profile.js
@@ -293,8 +293,8 @@ class Profile extends Component {
     file = file[0];
     var fr = new FileReader();
     fr.readAsDataURL(file);
-    console.log(fr);
-    console.log(fr['result'])
+    console.log(fr[0]);
+    console.log(fr['result']);
     this.setState({image : fr.result});
     console.log(this.state.image);
 
