76a369cf2f9befffe55d0e8649a25accd71b5bad
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-04-19 14:03:23 +0200

    Update profile.js

--- src/profile.js
+++ src/profile.js
@@ -302,7 +302,7 @@ AAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO
     9TXL0Y4OHwAAAABJRU5ErkJggg==" alt="Red dot" />
     <input type="file"
        id="avatar" name="avatar"
-       accept="image/png, image/jpeg" onChange={this.handleFiles} />
+       accept="image/png, image/jpeg" onChange={this.handleFiles(this.files)} />
     {this.processItems(this.state)}
     
     
