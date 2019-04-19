44e5a4e57627b922d1dec89ab7031ea964eb8570
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-04-19 13:51:17 +0200

    Update profile.js

--- src/profile.js
+++ src/profile.js
@@ -297,7 +297,9 @@ class Profile extends Component {
     <img src="data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUA
 AAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO
     9TXL0Y4OHwAAAABJRU5ErkJggg==" alt="Red dot" />
-    
+    <input type="file"
+       id="avatar" name="avatar"
+       accept="image/png, image/jpeg">
     {this.processItems(this.state)}
     
     
